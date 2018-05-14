5aad9e36da37ec728ee66398520caa3dba281f0a
KarapetyanSergo <sergo.karapetyan.00@mail.ru>
2018-05-14 14:43:30 -0700

    js code

--- Accelerometer/www/js/index.js
+++ Accelerometer/www/js/index.js
@@ -15,19 +15,21 @@ var app = {
             console.log(battery.level);
             $("#none").text(battery.level);
             var level = $("#none").text().replace('0.','');
-            $("#level").text(level + "%");
+            if(level.length == 1){
+                $("#level").text(level + "0%");
+            }
+            else{
+                $("#level").text(level + "%");
+            }
             if($("#none").text() > 0.7){
                 $("#lightning").addClass("green");
-                $("#lightning").text("Отлично")
             }
-            else if($("#none").text() < 0.7 && $("#none").text() > 0.2){
+            else if($("#none").text() <= 0.7 && $("#none").text() > 0.2){
                 $("#lightning").addClass("blue");
-                $("#lightning").text("Нормально")
             }
-            else if($("#none").text() < 0.2){
+            else if($("#none").text() <= 0.2){
                 $("#lightning").addClass("red");
-                $("#lightning").text("Плохо")
-            }
+            };
         });
     }
 };
